282cbdfda7db749798db38e5070695ea9b524cb1
coderhouseshop <coderhouseshop@gmail.com>
2023-09-26 19:34:56 -0300

    Alterar nome para celo express

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
             className="flex items-center mb-4 sm:mb-0"
           >
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Lama Express
+              Celo Express
             </span>
           </a>
           <div className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -23,7 +23,7 @@ export const Footer = () => {
             href="#"
             className="hover:underline"
           >
-            Lama Express
+            Celo Express
           </a>
           . Todos os direitos reservados
         </span>
